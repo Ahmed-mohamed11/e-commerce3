d47c30a018e7eba90c8ee71b915ecf879da6a081
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2024-10-10 12:50:02 +0300

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/home"
 import Navbar from "./components/Navbar"
 import Error404Modern from "./pages/error/404-modern"
 import { AdminPages } from "./route/Index"
 function App() {
 
   return (
-    <>
+    <div className="flex items-center  justify-between gap-10  overflow-x-hidden">
       <Navbar />
       <div className="pt-20 px-4 h-screen w-full bg-cover bg-no-repeat">
         <Routes>
           <Route path="/*" element={<AdminPages />} />
           <Route path="/*" element={<Error404Modern />} />
         </Routes>
-        <HomePage />
-      </>
-      )
+      </div>
+
+    </div >
+  )
 }
 
-      export default App
+export default App
